bb6686d57b4f3a30b31940d2d59464b23a9c2947
Neto test <veronikakaravanova60@gmail.com>
2025-12-14 15:29:58 +0500

    Initial commit: AutoQA8 project structure

---
+++ schema/schema.sql
@@ -0,0 +1,34 @@
+﻿CREATE TABLE users (
+    id VARCHAR(255) PRIMARY KEY,
+    login VARCHAR(255) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    status VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE auth_codes (
+    id VARCHAR(255) PRIMARY KEY,
+    user_id VARCHAR(255) NOT NULL,
+    code VARCHAR(255) NOT NULL,
+    created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES users(id)
+);
+
+CREATE TABLE cards (
+    id VARCHAR(255) PRIMARY KEY,
+    user_id VARCHAR(255) NOT NULL,
+    number VARCHAR(255) NOT NULL,
+    balance_in_kopecks INT NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users(id)
+);
+
+CREATE TABLE card_transactions (
+    id VARCHAR(255) PRIMARY KEY,
+    card_from_id VARCHAR(255) NOT NULL,
+    card_to_id VARCHAR(255) NOT NULL,
+    amount_in_kopecks INT NOT NULL,
+    FOREIGN KEY (card_from_id) REFERENCES cards(id),
+    FOREIGN KEY (card_to_id) REFERENCES cards(id)
+);
+
+INSERT INTO users (id, login, password, status)
+VALUES ('1', 'vasya', '$2a$10$bWDOJ6r4Q8JJH1Q8eKjumea2fBzOgPpe.9CiIh7DcJVoK3v1FJj0K', 'active');
---
+++ src/test/java/ru/netology/BankLoginTest.java
@@ -0,0 +1,49 @@
+﻿package ru.netology;
+
+import com.codeborne.selenide.Condition;
+import org.junit.jupiter.api.Test;
+
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class BankLoginTest {
+
+    @Test
+    void shouldLogin() throws SQLException {
+        var usersSql = "SELECT login, password FROM users WHERE login = 'vasya';";
+        var authCodesSql = "SELECT code FROM auth_codes ORDER BY created DESC LIMIT 1;";
+
+        try (
+            var conn = DriverManager.getConnection(
+                "jdbc:mysql://localhost:3306/app", "app", "pass"
+            );
+            var usersStmt = conn.createStatement();
+            var authStmt = conn.createStatement();
+        ) {
+            var usersRs = usersStmt.executeQuery(usersSql);
+            if (usersRs.next()) {
+                var login = usersRs.getString("login");
+                var password = usersRs.getString("password");
+                System.out.println("Login: " + login + ", Password hash: " + password);
+            }
+
+            open("http://localhost:9999");
+            $("[data-test-id=login] input").setValue("vasya");
+            $("[data-test-id=password] input").setValue("qwerty123");
+            $("[data-test-id=action-login]").click();
+
+            var authRs = authStmt.executeQuery(authCodesSql);
+            if (authRs.next()) {
+                var code = authRs.getString("code");
+                System.out.println("Code from DB: " + code);
+                $("[data-test-id=code] input").setValue(code);
+                $("[data-test-id=action-verify]").click();
+            }
+
+            $("h2").shouldHave(Condition.exactText("Личный кабинет"));
+        }
+    }
+}
